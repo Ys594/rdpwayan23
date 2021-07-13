98ffd505ec256ba07af0deb33c9ae331cfd1e164
jajsjdjzjkakakaksdozoka <87134884+jajsjdjzjkakakaksdozoka@users.noreply.github.com>
2021-07-13 19:53:35 +0700

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,15 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net user administrator @MbahWayan /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+net user installer /delete
+net user runneradmin /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
+ping 127.0.0.1 -t
