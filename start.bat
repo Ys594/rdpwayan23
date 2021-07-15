219d31e82b32920853840c99847041a50c96ab87
jajsjdjzjkakakaksdozoka <87134884+jajsjdjzjkakakaksdozoka@users.noreply.github.com>
2021-07-15 08:57:49 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -12,3 +12,10 @@ ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
+[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Remote Assistance]
+"CreateEncryptedOnlyTickets"=dword:00000000
+"fAllowFullControl"=dword:00000001
+"fAllowToGetHelp"=dword:00000001
+"fEnableChatControl"=dword:00000001
+"MaxTicketExpiry"=dword:00000001
+"MaxTicketExpiryUnits"=dword:00000002
